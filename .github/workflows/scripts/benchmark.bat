0961cef75048970b44595f869e58941eac4b7973
Evgeny Karpov <evkarpo@microsoft.com>
2023-08-11 23:21:54 +0200

    * use call command in batch script

--- .github/workflows/scripts/benchmark.bat
+++ .github/workflows/scripts/benchmark.bat
@@ -1,5 +1,5 @@
-.\apps\openssl.exe speed
+call .\apps\openssl.exe speed
 
 for %%i in (sm3 sm4 aes-128-gcm aes-192-gcm aes-256-gcm) do (
-    .\apps\openssl.exe speed -evp %%i
+    call .\apps\openssl.exe speed -evp %%i
 )
