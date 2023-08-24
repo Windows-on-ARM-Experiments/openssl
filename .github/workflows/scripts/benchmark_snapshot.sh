649f91b7a217828c6cc10ec1fd55c5a9febd6330
Evgeny Karpov <evkarpo@microsoft.com>
2023-08-24 15:02:39 +0200

    * change benchmark_snapshot folder location

--- .github/workflows/scripts/benchmark_snapshot.sh
+++ .github/workflows/scripts/benchmark_snapshot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-[ ! -d .github/workflows/data/benchmark_snapshot ] && mkdir -p .github/workflows/data/benchmark_snapshot
+[ ! -d .github/benchmark_snapshot ] && mkdir -p .github/benchmark_snapshot
 branch=$(git symbolic-ref --short HEAD)
-benchmark_snapshot=$(date +".github/workflows/data/benchmark_snapshot/${branch}_%Y-%m-%d_%H_%M_%S.txt")
+benchmark_snapshot=$(date +".github/benchmark_snapshot/${branch}_%Y-%m-%d_%H_%M_%S.txt")
 .github/workflows/scripts/benchmark_cmp.sh benchmark_aarch64_gcc.txt benchmark_aarch64_gcc.txt > $benchmark_snapshot || exit -1
 
 chart_payload="cht=bvg&chs=500x375&chtt=Perfromance%20ratio%20comparision&chma=30,30,30,30&chdlp=t&chco=4d89f9,c6d9fd&chbh=r,0,0&chxt=x,x,y&chxl=1:|Benchmark%20test%20%23&chxp=1,50&chds=a&chxs=2N*p&chdl=AArch64%20/%20Arm64%20Windows&chxr=0,0,247,48&chd=t:"
