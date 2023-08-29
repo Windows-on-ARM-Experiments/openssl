a5cd2f3ed4cc2d9c4d081c43d74618c6025f1d37
Evgeny Karpov <evkarpo@microsoft.com>
2023-08-29 18:59:26 +0200

    * convert CRLR to LR in Linux env

--- .github/workflows/scripts/benchmark_cmp.sh
+++ .github/workflows/scripts/benchmark_cmp.sh
@@ -3,8 +3,8 @@
 first_benchmark=$1
 second_benchmark=$2
 
-cat $first_benchmark | grep "^Doing" | sed -E "s/ +$//" > b1.txt
-cat $second_benchmark | grep "^Doing" | sed -E "s/ +$//" > b2.txt
+cat $first_benchmark | grep "^Doing" | sed -E "s/[ \r]+$//" > b1.txt
+cat $second_benchmark | grep "^Doing" | sed -E "s/[ \r]+$//" > b2.txt
 paste b1.txt b2.txt | awk '{match($0, /(^[^:]+)/, g1); match($0, /\t+(Doing[^:]+)/, g2); if (g1[1] != g2[1]) exit -1}' \
     || { echo "Benchmark test sets are not identical"; rm b1.txt b2.txt; exit -1; } 
 cat b2.txt | sed -E "s/^[^:]+: /\/\t/" | sed -E "s/ .* / /" > b2_1.txt
--- .github/workflows/scripts/benchmark_snapshot.sh
+++ .github/workflows/scripts/benchmark_snapshot.sh
@@ -3,7 +3,7 @@
 [ ! -d .github/benchmark_snapshot ] && mkdir -p .github/benchmark_snapshot
 branch=$(git symbolic-ref --short HEAD)
 benchmark_snapshot=$(date +".github/benchmark_snapshot/${branch}_%Y-%m-%d_%H_%M_%S_gcc_vs_clang.txt")
-.github/workflows/scripts/benchmark_cmp.sh benchmark_arm64_clangcl.txt benchmark_aarch64_gcc.txt > $benchmark_snapshot || exit -1
+. .github/workflows/scripts/benchmark_cmp.sh benchmark_arm64_clangcl.txt benchmark_aarch64_gcc.txt > $benchmark_snapshot || exit -1
 
 chart_payload="cht=bvg&chs=500x375&chtt=Perfromance%20ratio%20comparison&chma=30,30,30,30&chdlp=t&chco=4d89f9,c6d9fd&chbh=r,0,0&chxt=x,x,y&chxl=1:|Benchmark%20test%20%23&chxp=1,50&chds=a&chxs=2N*p&chdl=AArch64%20/%20Arm64%20Windows&chxr=0,0,247,48&chd=t:"
 chart_payload+=$(cat $benchmark_snapshot \
